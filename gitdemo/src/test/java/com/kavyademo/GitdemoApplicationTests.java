a6d817c46dad3a40a3567992fa328e1273090127
Kavya Cherukuri <kavyacherukuri21@gmail.com>
2026-05-07 17:12:24 +0530

    applicationtest

--- gitdemo/src/test/java/com/kavyademo/GitdemoApplicationTests.java
+++ gitdemo/src/test/java/com/kavyademo/GitdemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.kavyademo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GitdemoApplication.class)
 class GitdemoApplicationTests {
 
     @Test
